0619a6fc53ce4c9e24dbc00c2881cf7b690f700e
Your Name <you@example.com>
2025-11-01 23:02:23 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение списка заданий

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,6 +1,7 @@
 from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_schema import CreateExercisesRequestSchema, CreateExercisesResponseSchema, \
-    ExerciseSchema, GetExerciseResponseSchema, UpdateExercisesRequestSchema, UpdateExercisesResponseSchema
+    ExerciseSchema, GetExerciseResponseSchema, UpdateExercisesRequestSchema, UpdateExercisesResponseSchema, \
+    GetExercisesResponseSchema
 from tools.assertions.base import assert_equal
 from tools.assertions.errors import assert_internal_error_response
 
@@ -35,6 +36,21 @@ def assert_get_exercise_response(
     :raises AssertionError: Если данные заданий не совпадают.
     """
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
+
+
+def assert_get_exercises_response(
+        get_exercises_response: GetExercisesResponseSchema,
+        create_exercise_responses: list[CreateExercisesResponseSchema]
+):
+    """
+    Проверяет, что ответ на получение списка упражнений соответствует ответам на их создание.
+
+    :param get_exercises_response: Ответ API при запросе списка упражнений.
+    :param create_exercise_responses: Список API ответов при создании упражнений.
+    :raises AssertionError: Если данные упражнений не совпадают.
+    """
+    for index, create_exercise_response in enumerate(create_exercise_responses):
+        assert_exercise(get_exercises_response.exercises[index], create_exercise_response.exercise)
 
 
 def assert_create_exercise_response(
