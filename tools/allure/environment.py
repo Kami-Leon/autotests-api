370c799f9898c1275eb6c8584beb90ecbc5f4f60
Your Name <you@example.com>
2025-11-11 00:50:00 +0300

    11.4 Добавляем информацию о переменных окружения в Allure отчет. Практика работы с переменными окружения в Allure

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,12 +1,21 @@
+import sys
+import platform
+
 from config import settings
 
 
 def create_allure_environment_file():
-    # Создаем список из элементов в формате {key}={value}
-    items = [f'{key}={value}' for key, value in settings.model_dump().items()]
-    # Собираем все элементы в единую строку с переносами
+    os_info = f'{platform.system()}, {platform.release()}'
+    python_version = sys.version
+
+    environment_data = {
+        "os_info": os_info,
+        "python_version": python_version,
+        **settings.model_dump()
+    }
+
+    items = [f'{key}={value}' for key, value in environment_data.items()]
     properties = '\n'.join(items)
 
-    # Открываем файл ./allure-results/environment.properties на чтение
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
-        file.write(properties)  # Записываем переменные в файл
+        file.write(properties)
