1a15c0171dffb213853c0733d042ae48768cc81a
Your Name <you@example.com>
2025-11-11 16:50:21 +0300

    Улучшение работы с API клиентами с помощью Enum для URI

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -4,6 +4,7 @@ from httpx import Response
 from clients.api_client import ApiClient
 from clients.public_http_builder import get_public_http_client
 from clients.users.user_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tools.routes import APIRoutes
 
 
 class PublicUserClient(ApiClient):
@@ -19,7 +20,7 @@ class PublicUserClient(ApiClient):
         :param request: Словарь с данными нового пользователями
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("api/v1/users", json=request.model_dump(by_alias=True))
+        return self.post(f"{APIRoutes.USERS}", json=request.model_dump(by_alias=True))
 
     def create_user(self, request: CreateUserRequestSchema) -> CreateUserResponseSchema:
         response = self.create_user_api(request)
